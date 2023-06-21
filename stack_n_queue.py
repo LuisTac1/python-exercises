5619aafc0ef94d4e9c1494030257e9b39f6e9402
LuisTac1 <103211981+LuisTac1@users.noreply.github.com>
2023-06-21 08:34:02 -0300

    Add files via upload

---
+++ stack_n_queue.py
@@ -0,0 +1,19 @@
+"""
+Stacks and Rows
+Pilha - LIFO last in, first out.
+
+Queue - FIFO - first in, first out.
+    Example: A bank queue (or any real-life queue)
+    queues can have side effects on performance, because with each item changed 
+    all indices are modified.
+"""
+
+from collections import deque
+from time import sleep
+
+queue = deque(maxlen=10)
+
+for i in range(100):
+    queue.append(i)
+    sleep(1)
+    print(queue)
