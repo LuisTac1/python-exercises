7bb1fd8a20181d13178686ef596ece15f71bf049
LuisTac1 <103211981+LuisTac1@users.noreply.github.com>
2023-06-23 04:46:06 -0300

    Add files via upload

---
+++ process.py
@@ -0,0 +1,16 @@
+"""
+Subprocess - Subprocess management
+"""
+import subprocess
+
+# Windows - ping 127.0.0.1
+# Linux - ping 127.0.0.1
+
+proc = subprocess.run(
+    ['ping', '127.0.0.1'],
+    capture_output=True,
+    text=True
+)
+
+exi = proc.stdout
+print(exi)
