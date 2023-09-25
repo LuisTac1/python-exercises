31dc034679b893957a675248dca4475096728adb
Luis-Ed <103211981+LuisTac1@users.noreply.github.com>
2023-09-24 22:29:39 -0300

    Update zip_script.py

--- aut_zip/zip_script.py
+++ aut_zip/zip_script.py
@@ -8,8 +8,7 @@ path = ":\Your\Full\Path"
 with ZipFile('file.zip', 'w') as zip:
     for file in os.listdir(path):
         full_path = os.path.join(path, file)
-        zip.write(full_path, path)
-        
+        zip.write(full_path, path)        
 
     with ZipFile('file.zip', 'r') as zip:
         for file in zip.namelist():
